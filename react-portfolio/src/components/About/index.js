b230d6db0617531d408e9930567fdb5fa03d048f
Sifiso Patel <sfsptl42@gmail.com>
2022-04-21 17:56:50 +0200

    finished the about page and started on the loader but im having trouble with it entire page is not showing for some reason

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -2,7 +2,8 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular } from '@fortawesome/free-brands-svg-icons'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import { Loader } from 'react'
 
 const About = () => {
 
@@ -13,30 +14,50 @@ const About = () => {
             setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+
     return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>I'm a very ambitious front-end developer looking for a role in a established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.</p>
-                <p>I'm quietly confident, naturally curious, and always working on improving my skills one design problem at a time.</p>
-                <p>If I need to define myself in one sentence that would be a family person, sports fanatic, and tech-obsessed</p>
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>I'm a very ambitious front-end developer looking for a role in a established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.</p>
+                    <p>I'm quietly confident, naturally curious, and always working on improving my skills one design problem at a time.</p>
+                    <p>If I need to define myself in one sentence that would be a family person, sports fanatic, and tech-obsessed</p>
 
-            </div>
+                </div>
 
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
